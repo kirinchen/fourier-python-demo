ef9be083d001b2a2524178f25b78e87d22ab1ce0
kirin <kirin.chen1001@gmail.com>
2023-02-25 10:48:35 +0800

    add main_older.py

---
+++ fourier_python_demo/main_older.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants
+from scipy.fftpack import fft
+from scipy.fftpack import ifft
+
+if __name__ == "__main__":
+
+    sample_num = 2000  # Sampling points
+    total_time = 2  # Sampling number
+    sampling_rate = sample_num / total_time  # 取樣頻率
+
+    fs = [(20, 12), (100, 5), (250, 2)]  # sin 波的頻率與振幅組合。 (Hz, Amp)
+    noise_mag = 2
+
+    time = np.linspace(0, total_time, sample_num, endpoint=False)
+
+    vib_data = [amp * np.sin(2 * scipy.constants.pi * hz * time) for hz, amp in fs]
+
+    max_time = int(sample_num / 4)
+
+    plt.figure(figsize=(12, 8))
+    # Show seperated signal
+    for idx, vib in enumerate(vib_data):
+        plt.subplot(2, 2, idx + 1)
+        plt.plot(time[0:max_time], vib[0:max_time])
+        plt.xlabel('time')
+        plt.ylabel('vib_' + str(idx))
+        plt.ylim((-24, 24))
+
+    vib = sum(vib_data) + np.random.normal(0, noise_mag, sample_num)  # Add noise
+
+    plt.subplot(2, 2, 4)
+    plt.plot(time[0:max_time], vib[0:max_time])
+    plt.xlabel('time')
+    plt.ylabel('vib(with noise)')
+    plt.ylim((-24, 24))
+    plt.show()
+
+    fd = np.linspace(0.0, sampling_rate, int(sample_num), endpoint=False)
+
+    vib_fft = fft(vib)
+    mag = 2/sample_num * np.abs(vib_fft) # Magnitude
+
+    plt.plot(fd[0:int(sample_num/2)], mag[0:int(sample_num/2)])
+    plt.xlabel('Hz')
+    plt.ylabel('Mag')
+    plt.show()
+
+    vib_re = np.real(ifft(vib_fft))  # Real part of complex number
+
+    plt.plot(time[0:max_time], vib_re[0:max_time])
+    plt.ylim((-24, 24))
+    plt.show()
